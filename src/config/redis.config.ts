522d893c054c4f04a9a561ec9562a02a0d492fb9
Derrick Koko <kokoderrick3@gmail.com>
2026-01-18 13:45:53 +0100

    add dotenv dependency and debug logging for Redis configuration

--- src/config/redis.config.ts
+++ src/config/redis.config.ts
@@ -1,4 +1,9 @@
+import * as dotenv from 'dotenv';
 import { registerAs } from '@nestjs/config';
+
+dotenv.config();
+
+console.log(`REDIS_URL: ${process.env.REDIS_URL}`);
 
 export default registerAs('redis', () => ({
   url: process.env.REDIS_URL || 'redis://localhost:6379',
