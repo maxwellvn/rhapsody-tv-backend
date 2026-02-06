8a6c937650e54c4d5c5a2a884a55169053622350
Derrick Koko <kokoderrick3@gmail.com>
2026-02-06 19:06:31 +0100

    feat: add JwtStrategy for token-based authentication.

--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -25,6 +25,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 
   validate(payload: JwtPayload) {
     return {
+      sub: payload.sub,
       _id: payload.sub,
       email: payload.email,
       roles: payload.roles,
